d5a24248be75cb548f0c8381f6c079cc5ede981f
Rober Torres <robertdrakenfor@gmail.com>
2024-10-23 19:24:25 -0500

    Client: delete course

--- client/src/routes/admin/[faculty]/[school]/+page.svelte
+++ client/src/routes/admin/[faculty]/[school]/+page.svelte
@@ -4,6 +4,9 @@
 	import { page } from '$app/stores';
 	import { breadcrum } from '$lib/stores.js';
 	let course = '';
+
+	let course_delete = '';
+	let delete_id = -1;
 </script>
 
 <section class="courses">
@@ -30,6 +33,7 @@
 		{#each data.courses as course, id}
 			<li class="course">
 				<a
+					class={delete_id == id ? 'course-delete' : ''}
 					data-sveltekit-preload-data="false"
 					on:click={() => {
 						breadcrum.update((path) => [...path, { name: course.name, url: course.id }]);
@@ -41,10 +45,46 @@
 					<span class="enrolled">{course.enrolled}</span>
 					/
 					<span class="places">{course.places}</span>
+
+					<button on:click={() => (delete_id >= 0 ? (delete_id = -1) : (delete_id = id))}>
+						{delete_id == id ? 'Cancelar' : 'Eliminar'}
+					</button>
 				</div>
 			</li>
 		{/each}
 	</ul>
+
+	{#if delete_id >= 0}
+		<hr />
+		<div>
+			<form
+				method="POST"
+				class="delete"
+				action="?/delete"
+				use:enhance={() => {
+					return async ({ update }) => {
+						await update();
+						delete_id = -1;
+					};
+				}}
+			>
+				<input
+					bind:value={course_delete}
+					type="text"
+					placeholder="Para completar la eliminación ingrese el nombre del curso"
+				/>
+				<input type="text" name="course_id" value={data.courses[delete_id].id} hidden />
+				<button on:click={() => (delete_id = -1)}>Cancelar</button>
+				<button
+					class="btn-delete"
+					type="submit"
+					disabled={data.courses[delete_id].name != course_delete}
+				>
+					Eliminar
+				</button>
+			</form>
+		</div>
+	{/if}
 </section>
 
 <style>
@@ -60,7 +100,6 @@
 	.course {
 		margin-bottom: 3px;
 		background: var(--bg);
-		padding: 0.5em 1em;
 		border-radius: 5px;
 		display: flex;
 		justify-content: space-between;
@@ -69,6 +108,7 @@
 
 	.course-info {
 		min-width: 40px;
+		margin-right: 1em;
 		display: flex;
 		justify-content: space-between;
 	}
@@ -88,5 +128,52 @@
 		display: flex;
 		justify-content: space-between;
 		padding: 0.2em 0.5em;
+	}
+
+	a {
+		padding: 0.5em 1em;
+		width: 100%;
+	}
+
+	.delete {
+		width: 100%;
+		display: flex;
+		gap: 0.5em;
+	}
+
+	.delete input {
+		flex: 1;
+	}
+
+	.btn-delete {
+		background: red;
+	}
+
+	.btn-delete:hover {
+		background: darkred;
+	}
+
+	.btn-delete:disabled {
+		background: red;
+		opacity: 0.5;
+	}
+
+	.course-delete {
+		background: red;
+		color: white;
+		border-radius: 5px 0 0 5px;
+	}
+
+	.course-info button {
+		color: red;
+		background: initial;
+		padding: 0 2em 0 1em;
+		height: 0;
+		margin: 0;
+		width: 8em;
+	}
+
+	.course-info button:hover {
+		text-decoration: underline;
 	}
 </style>
